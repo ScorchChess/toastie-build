86df2db49a1b84e3e48dd3295cf3da26f14263a6
theLAZYmd <lazymeep+github@gmail.com>
2020-04-16 11:09:02 +0100

    Updates

---
+++ precache-manifest.f6bfb873264e5131278bc4f90ae0f52b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4970a4320107c994ebf0d36eaacf30e5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49215a267ebfc9a370c2",
+    "url": "./static/css/main.9e329e23.chunk.css"
+  },
+  {
+    "revision": "281650df69c2bc3e3361",
+    "url": "./static/js/2.45138964.chunk.js"
+  },
+  {
+    "revision": "426c49e9bf780a26b26257411baabdd9",
+    "url": "./static/js/2.45138964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49215a267ebfc9a370c2",
+    "url": "./static/js/main.512d2426.chunk.js"
+  },
+  {
+    "revision": "e759929849706c347a41",
+    "url": "./static/js/runtime-main.78a9d7b6.js"
+  },
+  {
+    "revision": "228db28eb4a3c4aa352f3fb02f688341",
+    "url": "./static/media/chessCom.228db28e.svg"
+  },
+  {
+    "revision": "9ff6e3058262c6adc0596b28b8279ddc",
+    "url": "./static/media/lichess.9ff6e305.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2e545fd8029622587a0a866e1bfa50fb.js"
+  "./precache-manifest.f6bfb873264e5131278bc4f90ae0f52b.js"
 );
 
 self.addEventListener('message', (event) => {
